7c18b6a4feadb3b1c59729895f58af770b16c193
Saqibur <saqibur.rahman98@gmail.com>
2020-05-12 21:18:41 +0600

    Update sample view

--- app/views/sample.php
+++ app/views/sample.php
@@ -6,27 +6,8 @@ echo $data;
 
 <head>
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <script src="libraries/jquery/jquery-3.4.1.min.js"></script>
-    <link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.min.css">
-    <script src="libraries/bootstrap/js/bootstrap.min.js"></script>
-    <link rel="icon" type="image/png" href="resources/logo.png">
-    <title>Gakkou - Home</title>
-    <style>
-        .tales {
-            width: 100%;
-        }
-
-        .carousel-inner,
-        .caroimg {
-            width: 100%;
-            max-height: 550px !important;
-        }
-    </style>
+    <title>Sample</title>
+    <?php echo $data; ?>
 
 </head>
 
