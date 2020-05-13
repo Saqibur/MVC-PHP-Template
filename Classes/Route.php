0d065b78c06771126e0c33a7219a171c0b17b631
Saqibur <saqibur.rahman98@gmail.com>
2020-05-14 02:53:14 +0600

    Trying something cool with the routes

--- Classes/Route.php
+++ Classes/Route.php
@@ -4,19 +4,25 @@ class Route
 {
     /**
      * @var array $routes is a list of valid routes for the entire application.
-     * @method void create($name, $function)adds a route to the valid route list.
+     * @method void Create($name, $function)adds a route to the valid route list.
      * @return void
      */
     public static $routes = array();
 
-    public static function create($name, $function)
+    public static function Create($name, $function)
     {
         /**
          * @param string $name is the name of the route, often used to invoke the route.
          * @param function $function is the callback function to invoke when the named route is called.
          */
 
-        self::$routes[] = $name;
+        
+        self::$routes[$name] = $name;
+
+        // TODO: IDK if this is the best way to do it.
+        // Yeah don't do this, it's not what actually happens and it's not the way to do it.
+        // self::$routes = array_unique(self::$routes);
+        
         if ($_GET['url'] == $name)
             $function->__invoke();
     }
