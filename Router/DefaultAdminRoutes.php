24a911abde49000fffac39bbc7d8c3ebae41e206
Saqibur <saqibur.rahman98@gmail.com>
2020-05-14 02:53:47 +0600

    Add default admin routes

--- Router/DefaultAdminRoutes.php
+++ Router/DefaultAdminRoutes.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
  * Admin Routes for a basic admin panel for your MVC application.
  * Feel free to get rid of everything.
  */
 
-Route::create('Admin', function () {
+Route::Create('Admin', function () {
     DefaultAdminController::Index();
 });
 
-Route::create("Admin/ShowAllRoutes", function () {
+Route::Create("Admin/ShowAllRoutes", function () {
     DefaultAdminController::ShowAllRoutes();
 });
