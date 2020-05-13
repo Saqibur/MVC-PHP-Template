32acb6f4e72c260bf23760dafa85d3c551f1d995
Saqibur <saqibur.rahman98@gmail.com>
2020-05-14 00:38:43 +0600

    Add test stuff

--- Views/TestView.php
+++ Views/TestView.php
@@ -1,3 +1,22 @@
 <?php
 echo "Inside TestView.";
 print_r($data);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./Resources/css/test.css">
+    <title>Document</title>
+</head>
+<body>
+
+<button class="btn btn-primary">Button</button>
+    
+</body>
+</html>
+
