e2d7387fea875da87d3c4ef19434d254e8e043fe
Saqibur <saqibur.rahman98@gmail.com>
2020-05-14 15:50:33 +0600

    Remove test routes

--- Router/Routes.php
+++ Router/Routes.php
@@ -5,3 +5,7 @@
  * To keep things clean, perform any and all modifications within your controllers.
  * If you need to pass around data, well I haven't figured that out yet.
  */
+
+
+ // TODO: FIX: Allows the creation of duplicate routes which are called one after another.
+
