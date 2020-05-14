9f170888f6edec9062267a93aaa61843680d67d6
Saqibur <saqibur.rahman98@gmail.com>
2020-05-14 15:51:42 +0600

    Clean up router

--- Router/Routes.php
+++ Router/Routes.php
@@ -4,6 +4,10 @@
  * Add your routes here.
  * To keep things clean, perform any and all modifications within your controllers.
  * If you need to pass around data, well I haven't figured that out yet.
+ * 
+ * You can add files and sub folders to the main router folder and require in
+ * those files within this php file for better organization.
+ * 
  */
 
 
